77cd41ea05e2ecadfa30890bcb47e6dacb572fda
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-02-21 21:16:02 +0100

    chore: update .gitignore and app.vue template

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+   Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Voluptate est accusantium ipsa alias quibusdam reiciendis dolorem aperiam perspiciatis? Totam et modi iste sunt voluptatum architecto rem dolorem necessitatibus reiciendis minus?
   </div>
 </template>
